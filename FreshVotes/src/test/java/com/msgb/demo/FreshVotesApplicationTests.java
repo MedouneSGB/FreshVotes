d89b2932a48be426db0f84d890718e312899e383
MSGB <medounesibygeorgesbalde@gmail.com>
2020-11-12 15:45:15 +0000

    Check initial de FreshVotes by MSGB

---
+++ FreshVotes/src/test/java/com/msgb/demo/FreshVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
